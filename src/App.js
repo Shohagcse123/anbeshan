858d849d9cd558181d88d7556ae26b740edf58ff
Arifakash16 <mdarifulislam6898@gmail.com>
2023-09-16 23:50:39 +0600

    search route working goog

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router,Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './Components/HomeScreen';
+import SearchScreen from './Components/SearchScreen';
+
 
 function App() {
   return (
-    <div className="App">
-       <Home/>
-    </div>
+    
+    <Router>
+      
+      <div className="App">
+        <Switch>
+          <Route exact path={"/"} component={()=><Home/>} />
+          <Route exact path={"/search"} component={()=> <SearchScreen/>}/>
+        </Switch>
+      </div>
+    </Router>
+    
   );
 }
 
---
+++ src/Components/SearchScreen/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchScreen = () => {
+  return (
+    <div className="Search_screen">
+      <h1>Welcome to search page</h1>
+    </div>
+  )
+}
+
+export default SearchScreen
